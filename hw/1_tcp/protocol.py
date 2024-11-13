48dfcd48f5c1d15d28f6d2e6f2e0160b7454151f
Aleksey <krasnov.as@phystech.edu>
2024-11-13 13:57:49 +0300

    almost done hw1

--- hw/1_tcp/protocol.py
+++ hw/1_tcp/protocol.py
@@ -37,26 +37,27 @@ class UDPBasedProtocol:
 
 no = 98
 def unpack_meta(meta):
-    segment_begin, size_and_request = struct.unpack('@iB', meta)
-    size = size_and_request & 127
-    request = (size_and_request & 128) > 0
+    segment_begin, size_and_request = struct.unpack('@ih', meta)
+    size = size_and_request & 1023
+    request = (size_and_request & 1024) > 0
     return segment_begin, segment_begin + size, request
 
 class MyTCPProtocol(UDPBasedProtocol):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.segment_size = 20
-        self.send_buffer = np.ndarray(5000000, dtype = 'byte')
+        self.udp_socket.settimeout(0.00001)
+        self.segment_size = 0
+        self.send_buffer = np.ndarray(10000000, dtype = 'byte')
         self.send_start = 0
         self.recv_start = 0
-        self.recv_buffer = np.ndarray(5000000, dtype = 'byte')
-        self.valid = np.zeros(5000000, dtype = 'bool')
-        self.padding = np.zeros(self.segment_size, dtype = 'byte').tobytes()
+        self.recv_buffer = np.ndarray(10000000, dtype = 'byte')
+        self.valid = np.zeros(10000000, dtype = 'bool')
+        self.padding = np.zeros(1000, dtype = 'byte').tobytes()
 
         self.running = True
         self.eventEnd = threading.Event()
-        self.recv_buffer_lock = threading.Lock()
-        self.send_buffer_lock = threading.Lock()
+        # self.recv_buffer_lock = threading.Lock()
+        # self.send_buffer_lock = threading.Lock()
         self.listener = threading.Thread(target=self._listener_work)
         self.initialized = False
         global no
@@ -65,6 +66,10 @@ class MyTCPProtocol(UDPBasedProtocol):
 
         self.sendtry = 0
         self.recvtry = 0
+        self.waitForSegmentBegin = None
+        self.waitForSegmentEnd = None
+        self.waitForSegmentEvent = threading.Event()
+        self.waitForSegmentEvent.set()
 
 
     def _listener_work(self):
@@ -72,40 +77,54 @@ class MyTCPProtocol(UDPBasedProtocol):
         while self.running:
             if self.eventEnd.is_set():
                 return
-            segment = self.recvfrom(self.segment_size + 5)
-            segment_begin, segment_end, is_request = unpack_meta(segment[:5])
-            # print(self.id, "recv", self.recvtry, segment_begin, segment_end, is_request)
-
-            if not is_request:
-                if not self.valid[segment_begin]:
-                    data_array = np.frombuffer(segment[5 : segment_end - segment_begin + 5], dtype=self.recv_buffer.dtype)
-                    # print(self.id, "recv",  self.recvtry, data_array)
-                    with self.recv_buffer_lock:
-                        self.recv_buffer[segment_begin : segment_end] = data_array
-                        self.valid[segment_begin] = True # ?
+            try:
+                # print('start listen', self.id)
+                segment = self.recvfrom(self.segment_size + 6)
+            except:
+                if not self.waitForSegmentEvent.is_set():
+                    self._send_segment(self.waitForSegmentBegin, self.waitForSegmentEnd, True)
+                    # print(self.id, "send request", self.recvtry, self.waitForSegmentBegin, self.waitForSegmentEnd)
             else:
-                if segment_begin < highier_send_begin:
-                    continue
-                # with self.send_buffer_lock:
-                if self.send_start < segment_end:
-                    continue
-                self._send_segment(segment_begin, segment_end, False)
-                highier_send_begin = segment_begin
-            self.recvtry += 1
+                segment_begin, segment_end, is_request = unpack_meta(segment[:6])
+                # print(self.id, "recv", self.recvtry, segment_begin, segment_end, is_request)
+
+                if not is_request:
+                        if not self.valid[segment_begin]:
+                            data_array = np.frombuffer(segment[6 : segment_end - segment_begin + 6], dtype=self.recv_buffer.dtype)
+                            # print(self.id, "recv",  self.recvtry, data_array)
+                            self.recv_buffer[segment_begin : segment_end] = data_array
+                            self.valid[segment_begin] = True # ?
+
+                        if not self.waitForSegmentEvent.is_set() and segment_begin == self.waitForSegmentBegin:
+                            self.waitForSegmentEvent.set()
+                else:
+                    # if segment_begin == segment_end:
+                    #     self._send_segment(segment_begin + 1, segment_begin + 1, True)
+                    #     self.segment_size = segment_begin
+
+                    if segment_begin < highier_send_begin:
+                        continue
+                    # with self.send_buffer_lock:
+                    if self.send_start >= segment_end:
+                        self._send_segment(segment_begin, segment_end, False)
+                        # print(self.id, "send data", self.recvtry, segment_begin, segment_end)
+                    highier_send_begin = segment_begin
+                # self.recvtry += 1
                 
 
 
     def _send_segment(self, begin, end, is_request):
-        # print(self.id, "send", self.sendtry ,begin, end, 1 if is_request else 0)
+        # print(self.id, "send", self.sendtry ,begin, end, 1 if is_request else 0, file=sys.stderr)
         size_and_request = end - begin
-        size_and_request += 128 if is_request else 0
-        segment = struct.pack('@iB', begin, size_and_request)   # meta info
+        size_and_request += 1024 if is_request else 0
+        segment = struct.pack('@ih', begin, size_and_request)   # meta info
         if not is_request:
             # print(self.id, "send", self.sendtry, self.send_buffer[begin : end])
             data = self.send_buffer[begin : end].tobytes()
             segment += data
-        self.sendtry += 1
-        segment += self.padding[:self.segment_size + 5 - len(segment)]
+        # self.sendtry += 1
+        segment += self.padding[:self.segment_size + 6 - len(segment)]
+        
         self.sendto(segment)
         
 
@@ -114,6 +133,7 @@ class MyTCPProtocol(UDPBasedProtocol):
         num_segments = data_size / self.segment_size
         if int(num_segments) != num_segments:
             num_segments = int(num_segments) + 1
+        num_segments = int(num_segments)
 
         for i in range(num_segments):
             segment_begin = i * self.segment_size 
@@ -121,43 +141,40 @@ class MyTCPProtocol(UDPBasedProtocol):
             self._send_segment(segment_begin, segment_end, False)
     
     def send(self, data: bytes):
+        # print(self.id, "send",  file=sys.stderr)
+        data_array = np.frombuffer(data, dtype=self.send_buffer.dtype)
         if not self.initialized:
+            self.segment_size = min(data_array.shape[0], 1000)
             self.listener.start()
             self.initialized = True
-
-        data_array = np.frombuffer(data, dtype=self.send_buffer.dtype)
+            # self._send_segment(data_array.shape[0], data_array.shape[0], True)
 
         begin = self.send_start
         end = self.send_start + data_array.shape[0]
 
-        with self.send_buffer_lock:
-            self.send_buffer[begin : end] = data_array
-            self.send_start += data_array.shape[0]
+        # with self.send_buffer_lock:
+        self.send_buffer[begin : end] = data_array
+        self.send_start += data_array.shape[0]
 
-        # self._send_data(begin, end)
+        self._send_data(begin, end)
 
         return data_array.shape[0]
 
     def _validate_segment(self, begin, end):
-        # with self.recv_buffer_lock:
-        # valid = self.valid[begin]
         
-        while not self.valid[begin]:
-            self._send_segment(begin, end, True)
-            time.sleep(0.01)
-            i += 1
-            assert i < 20
-            # sleep_time = min(sleep_time * 1.5, 0.02)
-
-            # with self.recv_buffer_lock:
-            # valid = self.valid[begin]
+        if self.waitForSegmentEvent.is_set() and not self.valid[begin]:
+            self.waitForSegmentBegin = begin
+            self.waitForSegmentEnd = end
+            self.waitForSegmentEvent.clear()
+            self.waitForSegmentEvent.wait()
 
     def _validate_data(self, data_begin, data_end):
         data_size = (data_end - data_begin)
         num_segments = data_size / self.segment_size
         if int(num_segments) != num_segments:
             num_segments = int(num_segments) + 1
-        
+        num_segments = int(num_segments)
+
         for i in range(num_segments):
             segment_begin = data_begin + i * self.segment_size 
             segment_end   = data_begin + min((i + 1) * self.segment_size, data_size)
@@ -165,11 +182,12 @@ class MyTCPProtocol(UDPBasedProtocol):
     
     def recv(self, n: int):
         if not self.initialized:
+            self.segment_size = min(n, 1000)
             self.listener.start()
             self.initialized = True
-            if self.id == 99:
-                time.sleep(0.1)
+           
         self._validate_data(self.recv_start, self.recv_start + n)
+        # with self.recv_buffer_lock:
         data = self.recv_buffer[self.recv_start : self.recv_start + n].tobytes()
         self.recv_start += n
         return data
